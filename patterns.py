e1af18ff11948567ac8ef3be04c058a27deea739
Navadeep Bommarapu <bommarapunavadeep@gmail.com>
2025-03-08 16:07:41 +0530

    patterns

---
+++ patterns.py
@@ -0,0 +1,69 @@
+# Patterns in python
+
+# for i in range(5):
+#     for j in range(5):
+#         print("*", end="")
+#     print()
+# output:
+# *****
+# *****
+# *****
+# *****
+# *****
+
+
+# for i in range(5):
+#     for j in range(i+1):
+#         print("*", end="")
+#     print()
+# output:
+# *
+# **
+# ***
+# ****
+# *****
+
+
+# n = 5
+# for i in range(1,n+1):
+#     for j in range(1,i+1):
+#         print(j, end='')
+#     print()
+# output:
+# 1
+# 12
+# 123
+# 1234
+# 12345
+
+
+# n = 5
+# for i in range(1,n+1):
+#     for j in range(1,i+1):
+#         print(i, end="")
+#     print()
+# output:
+# 1
+# 22
+# 333
+# 4444
+# 55555
+
+
+# n = 5
+# for i in range(n,0,-1):
+#     for j in range(i):
+#         print("*", end="")
+#     print()
+# Output:
+# *****
+# ****
+# ***
+# **
+# *
+
+n = 5
+for i in range(n,0,-1):
+    for j in range(n):
+        print(" ", end="")
+    print("*")
