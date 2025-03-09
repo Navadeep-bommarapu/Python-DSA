d575cb936cd5963e49e271fe84179a0d032e2092
Navadeep Bommarapu <bommarapunavadeep@gmail.com>
2025-03-09 14:06:10 +0530

    patterns

--- patterns.py
+++ patterns.py
@@ -63,3 +63,97 @@ for i in range(n,0,-1):
 # *
 
 
+n = 5
+for i in range(n):
+    for j in range(n-i-1,0,-1):
+        print(" ",end="")
+    for k in range(2*i+1):
+        print("*",end="")
+    print()
+# output:
+#     *
+#    ***
+#   *****
+#  *******
+# *********
+
+
+n = 5
+for i in range(n,0,-1):
+    for j in range(n,i,-1):
+        print(" ",end="")
+    for k in range(2*i-1):
+        print("*",end="")
+    print()
+# output:
+# *********
+#  *******
+#   *****
+#    ***
+#     *
+
+n = 5
+for i in range(n):
+    for j in range(n-i-1,0,-1):
+        print(" ",end="")
+    for k in range(2*i+1):
+        print("*", end="")
+    print()
+for i in range(n,0,-1):
+    for j in range(n,i,-1):
+        print(" ",end="")
+    for k in range(2*i-1):
+        print("*", end="")
+    print()
+# output:
+#     *
+#    ***
+#   *****
+#  *******
+# *********
+# *********
+#  *******
+#   *****
+#    ***
+#     *        
+
+n = 5
+for i in range(n+1):
+    for j in range(i):
+        print("*", end="")
+    print()
+for i in range(n,0,-1):
+    for j in range(i):
+        print("*", end="")
+    print()
+# output:
+# *
+# **
+# ***
+# ****
+# *****
+# *****
+# ****
+# ***
+# **
+# *
+
+n = 5
+for i in range(1,n+1):
+    for j in range(1,i+1):
+        if (i+j) % 2 == 0: 
+            print("1", end="")  
+        else: 
+            print("0",end="") 
+    print()
+# output:
+# 1
+# 01
+# 101
+# 0101
+# 10101
+
+
+
+
+    
